e733f2ca36aaed024e7e0ed463d1638ef8fcccea
Simuch <Simuch@DESKTOP-OGUHHPP.station>
2024-06-30 21:34:34 +0200

    piccolo change parte 2 grazie silvy

--- src/main/webapp/js/carrello.js
+++ src/main/webapp/js/carrello.js
@@ -1,24 +1,24 @@
 
-// Funzione per aggiornare il totale
+// funzione per aggiornare il totale
 function aggiornaTotale() {
     var spedizione = document.getElementById("spedizione").value;
 
-    // Verifica se il valore della spedizione è valido
+    // verifica se il valore della spedizione è valido
     if (!isNaN(parseFloat(spedizione))) {
         var totale = parseFloat(document.getElementById("totalPrice").value);
 
 
-        // Aggiungi il costo della spedizione al totale
+        // aggiungi il costo della spedizione al totale
         totale += parseFloat(spedizione);
 
-        // Formatta e visualizza il nuovo totale
+        // rende decente il totale e lo mostra oppure da errore
         document.getElementById("totale").innerHTML = "&euro; " + totale.toFixed(2);
     } else {
         console.error("Il valore della spedizione non è valido.");
     }
 }
 
-// Chiama la funzione aggiornaTotale() al caricamento della pagina
+// chiama la funzione aggiornaTotale() al caricamento della pagina
 window.onload = function() {
     aggiornaTotale();
 };
